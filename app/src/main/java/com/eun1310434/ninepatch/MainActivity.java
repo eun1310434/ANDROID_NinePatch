1136b0731013b0f4b3611d7c1c66b3f9bbe2f13a
eun1310434 <eun1310434@naver.com>
2018-03-07 23:33:17 +0900

    나인페치를 활용하여 맞춤형 이미지 형성

---
+++ app/src/main/java/com/eun1310434/ninepatch/MainActivity.java
@@ -0,0 +1,27 @@
+/*=====================================================================
+□ Infomation
+  ○ Data : 07.03.2018
+  ○ Mail : eun1310434@naver.com
+  ○ Blog : https://blog.naver.com/eun1310434
+  ○ Reference : Do it android app Programming
+
+□ Function
+  ○
+
+□ Study
+  ○
+=====================================================================*/
+
+package com.eun1310434.ninepatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
